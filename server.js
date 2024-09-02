c6dc5fdd775a593d596a35116cdf4ecafef7ffa3
enricodelguerra <ricodelguerra@gmail.com>
2024-09-02 18:24:57 -0300

    Estrutura JSON criada

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const http = require('http');
 
+const dadosPessoais = {
+    nome: "Enrico Figueiredo Del Guerra",
+    idade: 18,
+    descrição: "Sou uma pessoa que gosta de aprender com os outros e ensinar quando possivel, sou comunicativo. ",
+    gênero: "Masculino",
+    país: "Brasil"
+} 
+
 // Criando o servidor
 const server = http.createServer((req, res) => {
   // Definindo cabeçalhos de resposta
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { 'Content-Type': 'text/json' });
 
   // Enviando uma resposta HTML
   res.end(
-    
+    JSON.stringify(dadosPessoais)
   );
 });
 
